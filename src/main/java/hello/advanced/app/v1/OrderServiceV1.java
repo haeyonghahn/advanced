8a7e5adb46686393b78187e95886ef30468b5cd7
haeyonghahn <yong80211@gmail.com>
2023-04-28 19:34:00 +0900

    feat: 필드 동기화 - 개발

--- src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -1,7 +1,7 @@
 package hello.advanced.app.v1;
 
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.helloTraceV1.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
--- src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -1,4 +1,4 @@
-package hello.advanced.trace.helloTraceV1;
+package hello.advanced.trace.hellotrace;
 
 import hello.advanced.trace.TraceStatus;
 import org.junit.jupiter.api.Test;
